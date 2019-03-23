402d3fba119bf78d21fda8b70086b11def7b5b52
millan-figueroa <millan.fig@gmail.com>
2019-03-23 10:54:16 -0700

    Imported api data set and added styles to app.css.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -15,10 +15,7 @@ class App extends Component {
   }
 
   getCharacters = (URL) => {
-
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-
-    fetch(proxyUrl + URL)
+    
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
@@ -37,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">React Wars Character Cards</h1>
         <CharacterCards characters={this.state.starwarsChars}/>
       </div>
     );
